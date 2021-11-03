d9606a59afb9606b1f2b8b7c1b909dbfb1b30bc5
Charlie Larsson <charlie.larsson@realgymnasiet.se>
2021-11-04 00:03:54 +0100

    Testobjekt framtaget, collision kod intaget

--- GYMARB PacMan/Game1.cs
+++ GYMARB PacMan/Game1.cs
@@ -13,6 +13,9 @@ namespace GYMARB_PacMan
         Texture2D pmTexture;
         Vector2 pmPosition;
         Vector2 pmSpeed;
+        Texture2D test;
+        bool Hit;
+        char pmDirection;
 
         public Game1()
         {
@@ -38,6 +41,9 @@ namespace GYMARB_PacMan
             spriteBatch = new SpriteBatch(GraphicsDevice);
             pmTexture = Content.Load<Texture2D>("Pacman");
 
+
+            test = Content.Load<Texture2D>("Test");
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -46,33 +52,66 @@ namespace GYMARB_PacMan
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            // Pacmans rörelse
+
             pmPosition += pmSpeed;
 
             
 
             var state = Keyboard.GetState();
+
             if (state.IsKeyDown(Keys.W))
             {
-                pmSpeed = new Vector2(0, -2.0f);
+                pmSpeed = new Vector2(0, -1.0f);
+                pmDirection = 'W';
+
             }
             else if (state.IsKeyDown(Keys.D))
             {
-                pmSpeed = new Vector2(2.0f, 0);
+                pmSpeed = new Vector2(1.0f, 0);
+                pmDirection = 'D';
+
             }
             else if (state.IsKeyDown(Keys.A))
             {
-                pmSpeed = new Vector2(-2.0f, 0);
+                pmSpeed = new Vector2(-1.0f, 0);
+                pmDirection = 'A';
+
             }
             else if (state.IsKeyDown(Keys.S))
             {
-                pmSpeed = new Vector2(0, 2.0f);
+                pmSpeed = new Vector2(0, 1.0f);
+                pmDirection = 'S';
+
             }
             else
             {
                 pmSpeed = new Vector2(0, 0);
             }
 
-            // TODO: Add your update logic here
+
+
+            //kollision
+
+            Rectangle pmBox = new Rectangle((int)pmPosition.X, (int)pmPosition.Y, pmTexture.Width, pmTexture.Height);
+            Rectangle testBox = new Rectangle((int)200, (int)200, 512, 512);
+
+
+            var kollision = Intersection(pmBox, testBox);
+
+            if (kollision.Width > 0 && kollision.Height > 0)
+            {
+                Rectangle r1 = Normalize(pmBox, kollision);
+                Rectangle r2 = Normalize(testBox, kollision);
+                Hit = TestCollision(pmTexture, r1, test, r2);
+                if (state.IsKeyDown(Keys.W))
+                {
+
+                }
+            }
+
+
+
 
             base.Update(gameTime);
         }
@@ -83,12 +122,70 @@ namespace GYMARB_PacMan
 
             spriteBatch.Begin();
             spriteBatch.Draw(pmTexture, pmPosition, Color.White);
+            spriteBatch.Draw(test, new Vector2 (200,200), Color.White);
             spriteBatch.End();
+
+
+
+
 
 
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+
+
+
+
+
+
+
+
+
+        public static Rectangle Intersection(Rectangle r1, Rectangle r2)
+        {
+            int x1 = Math.Max(r1.Left, r2.Left);
+            int y1 = Math.Max(r1.Top, r2.Top);
+            int x2 = Math.Min(r1.Right, r2.Right);
+            int y2 = Math.Min(r1.Bottom, r2.Bottom);
+
+            if ((x2 >= x1) && (y2 >= y1))
+            {
+                return new Rectangle(x1, y1, x2 - x1, y2 - y1);
+            }
+            return Rectangle.Empty;
+        }
+        public static Rectangle Normalize(Rectangle reference, Rectangle overlap)
+        {
+            //Räkna ut en rektangel som kan användas relativt till referensrektangeln
+            return new Rectangle(
+              overlap.X - reference.X,
+              overlap.Y - reference.Y,
+              overlap.Width,
+              overlap.Height);
+        }
+        public static bool TestCollision(Texture2D t1, Rectangle r1, Texture2D t2, Rectangle r2)
+        {
+            //Beräkna hur många pixlar som finns i området som ska undersökas
+            int pixelCount = r1.Width * r1.Height;
+            uint[] texture1Pixels = new uint[pixelCount];
+            uint[] texture2Pixels = new uint[pixelCount];
+
+            //Kopiera ut pixlarna från båda områdena
+            t1.GetData(0, r1, texture1Pixels, 0, pixelCount);
+            t2.GetData(0, r2, texture2Pixels, 0, pixelCount);
+
+            //Jämför om vi har några pixlar som överlappar varandra i områdena
+            for (int i = 0; i < pixelCount; ++i)
+            {
+                if (((texture1Pixels[i] & 0xff000000) > 0) && ((texture2Pixels[i] & 0xff000000) > 0))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
