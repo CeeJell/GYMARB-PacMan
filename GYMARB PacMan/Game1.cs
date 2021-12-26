12edc28b08d36226dee202cf16522e5bff7b2706
Charlie Larsson <charlie.larsson@realgymnasiet.se>
2021-12-26 18:57:37 +0100

    lagt in spöke(ingen funktion), bytte så att coins använder samma kollisionkod som vägg, samt lade in vägg i array och lade in en andra testvägg

--- GYMARB PacMan/Game1.cs
+++ GYMARB PacMan/Game1.cs
@@ -17,11 +17,15 @@ namespace GYMARB_PacMan
         float pmSpeed = 3f;
         Vector2 pmVelocity;
 
+        Texture2D gRedTexture;
+        Vector2 gRedPosition = new Vector2(100, 400);
+        Rectangle gRedBox;
+
         Texture2D coinTexture;
         Vector2 coinPosition = new Vector2(200, 20);
 
         Texture2D testTexture;
-        Rectangle wall;
+        Vector2 wallPosition = new Vector2(300, 300);
         SpriteFont font;
         int points = 0;
 
@@ -54,6 +58,7 @@ namespace GYMARB_PacMan
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             pmTexture = Content.Load<Texture2D>("Pacman");
+            gRedTexture = Content.Load<Texture2D>("GhostRed");
             coinTexture = Content.Load<Texture2D>("Coin");
             testTexture = Content.Load<Texture2D>("Test");
             font = Content.Load<SpriteFont>("font");
@@ -67,8 +72,10 @@ namespace GYMARB_PacMan
             }
 
 
-
-            wall = new Rectangle(300, 300, testTexture.Width, testTexture.Height);
+            
+            walls.Add(new Rectangle((int)wallPosition.X, (int)wallPosition.Y, testTexture.Width, testTexture.Height));
+            wallPosition = new Vector2(350, 350);
+            walls.Add(new Rectangle((int)wallPosition.X, (int)wallPosition.Y, testTexture.Width, testTexture.Height));
 
         }
         
@@ -101,24 +108,12 @@ namespace GYMARB_PacMan
                 pmBox.Left < touch.Right;
         }
         
-        public static Rectangle Intersection(Rectangle r1, Rectangle r2)
-        {
-            int x1 = Math.Max(r1.Left, r2.Left);
-            int y1 = Math.Max(r1.Top, r2.Top);
-            int x2 = Math.Min(r1.Right, r2.Right);
-            int y2 = Math.Min(r1.Bottom, r2.Bottom);
-
-            if ((x2 >= x1) && (y2 >= y1))
-        {
-            return new Rectangle(x1, y1, x2 - x1, y2 - y1);
-        }
-            return Rectangle.Empty;
-        }
 
         protected override void Update(GameTime gameTime)
         {
             pmBox = new Rectangle((int)pmPosition.X, (int)pmPosition.Y, 20, 20);
 
+            gRedBox = new Rectangle((int)gRedPosition.X, (int)gRedPosition.Y, 18, 18);
             
 
             if (Keyboard.GetState().IsKeyDown(Keys.A))
@@ -130,27 +125,29 @@ namespace GYMARB_PacMan
             else if (Keyboard.GetState().IsKeyDown(Keys.S))
                 pmVelocity.Y = pmSpeed;
 
-            if (pmVelocity.X > 0 && TouchingLeft(wall) || pmVelocity.X < 0 && TouchingRight(wall))
-                pmVelocity.X = 0;
-            if (pmVelocity.Y > 0 && TouchingTop(wall) || pmVelocity.Y < 0 && TouchingBottom(wall))
-                pmVelocity.Y = 0;
+            foreach (var wall in walls)
+            {
+                if (pmVelocity.X > 0 && TouchingLeft(wall) || pmVelocity.X < 0 && TouchingRight(wall))
+                    pmVelocity.X = 0;
+                if (pmVelocity.Y > 0 && TouchingTop(wall) || pmVelocity.Y < 0 && TouchingBottom(wall))
+                    pmVelocity.Y = 0;
+            }
+
 
             pmPosition += pmVelocity;
             pmVelocity = Vector2.Zero;
 
-
-
             int i = 0;
-                foreach (var coin in coins)
+            foreach (var coin in coins)
+            {
+                if (TouchingLeft(coin) || TouchingRight(coin) || TouchingTop(coin) || TouchingBottom(coin))
                 {
-                    if (pmBox.Intersects(coin))
-                    {
-                        points++;
-                        coins.RemoveAt(i);
-                        break;
-                    }
-                i++;
+                    points++;
+                    coins.RemoveAt(i);
+                    break;
                 }
+                i++;
+            }
             i = 0;
 
 
@@ -168,7 +165,11 @@ namespace GYMARB_PacMan
 
             spriteBatch.Draw(pmTexture, pmPosition, Color.White);
 
-            spriteBatch.Draw(testTexture, new Vector2(300, 300), Color.Red);
+            foreach (var wall in walls)
+            {
+                spriteBatch.Draw(testTexture, wall, Color.Red);
+            }
+
 
             foreach (var coin in coins)
             {
@@ -176,6 +177,8 @@ namespace GYMARB_PacMan
             }
 
             spriteBatch.DrawString(font, points.ToString(), new Vector2(10, 20), Color.White);
+
+            spriteBatch.Draw(gRedTexture, gRedPosition, Color.White);
 
             spriteBatch.End();
 
